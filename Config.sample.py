8e74cd94b790ac2f9e0b8057090d73a8dc49cea6
Achim Domma <domma@localhost.(none)>
2010-12-29 16:02:12 +0100

    First sample for magento to xml export.

---
+++ Config.sample.py
@@ -0,0 +1,6 @@
+# remove sample for the filename, but never add a file to Git which
+# contains login data!!!
+
+url = 'http://login:pwd@www.saarlouis.com'
+apiuser = 'login'
+apipass = 'pwd'
--- MagentoExport.py
+++ MagentoExport.py
@@ -1,2 +1,14 @@
-__author__ = 'domma'
-  
+from magento import Customer
+from magento.api import API
+
+from lxml import etree
+from lxml.builder import E
+
+from Config import *
+
+with API(url, apiuser,apipass) as api:
+    orders = api.call('sales_order.list',[])
+    xml = E.orders(
+        *[E.order(*[getattr(E,name)(value) for name,value in order.items() if value]) for order in orders]
+    )
+    file('out.xml','w').write(etree.tostring(xml, encoding='iso-8859-1', pretty_print=True))
